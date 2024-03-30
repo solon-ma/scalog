67c2f78cf28e1dae4e475402f6d5581c5ba28b05
Solon Ma <solonma@berkeley.edu>
2024-03-29 23:04:29 -0700

    updated go dependency

--- order/order_server.go
+++ order/order_server.go
@@ -10,7 +10,7 @@ import (
 	"github.com/scalog/scalog/order/orderpb"
 
 	"github.com/golang/protobuf/proto"
-	"go.etcd.io/etcd/etcdserver/api/snap"
+	"go.etcd.io/etcd/server/v3/etcdserver/api/snap"
 	"go.etcd.io/etcd/raft/raftpb"
 )
 
---
+++ test.go
@@ -0,0 +1,13 @@
+package main 
+
+import ( 
+	"fmt"
+	"scalog/client"
+	
+	
+)
+
+func main() {
+	print("Hello World\n")
+
+}
